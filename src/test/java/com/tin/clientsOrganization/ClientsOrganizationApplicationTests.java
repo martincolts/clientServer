0115c70254f16a0b260758069d57b21cc2f4995f
tincho <martinlopez89.quequen@gmail.com>
2018-04-30 03:35:30 -0300

    fix test

--- src/test/java/com/tin/clientsOrganization/ClientsOrganizationApplicationTests.java
+++ src/test/java/com/tin/clientsOrganization/ClientsOrganizationApplicationTests.java
@@ -2,14 +2,17 @@ package com.tin.clientsOrganization;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 public class ClientsOrganizationApplicationTests {
 
 	@Test
